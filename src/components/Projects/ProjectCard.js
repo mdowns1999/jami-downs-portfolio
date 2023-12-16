0551eaa86d162c6999d4cd257b545a4adee55a7d
mdowns <michaeljdowns99@gmail.com>
2023-12-16 10:05:58 -0700

    Finished Site for now

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import classes from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
+  let name = props.name.charAt(0).toUpperCase() + props.name.slice(1);
   return (
     <Link to={`${props.link}`}>
-      <div className={classes.projectCard}>{props.name.toUpperCase()}</div>
+      <div className={classes.projectCard}>{name}</div>
     </Link>
   );
 };
